e907cf8d6b2c33dfb83a53ee5ead098c8e40ad66
Jheron Dylan Conde Martinez <jheronconde032@gmail.com>
2025-05-08 23:53:42 -0400

    corrigiendo nuevos erores

--- CalculadoraConsola.java
+++ CalculadoraConsola.java
@@ -119,7 +119,7 @@ public class CalculadoraConsola {
     private int factorial(int n){
       int res;
       if (n==0)
-          res=0;
+          res=1;
       else
         res=n*factorial(n-1);  
      return res ;
